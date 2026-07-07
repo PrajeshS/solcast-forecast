9620ed43b7b3b459837ee1e6ca82d0d0dfdb1e22
PrajeshS <89408105+PrajeshS@users.noreply.github.com>
2026-07-07 11:04:50 +0530

    Update solcast_api.py

--- solcast_api.py
+++ solcast_api.py
@@ -1,62 +1,44 @@
 import requests
-import csv
+import pandas as pd
 import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-# API endpoint
-url = "https://api.solcast.com.au/data/forecast/premium_pv_power"
+API_KEY = "Ltmj-pqWaCVKTAni0yrsexlj3ZKMUv_S"
 
-# Parameters
+url = "https://api.solcast.com.au/data/forecast/radiation_and_weather"
 
 params = {
-    "resource_id": "7f72-69a6-9138-089",
-    "api_key": "Ltmj-pqWaCVKTAni0yrsexlj3ZKMUv_S",
-    "output_parameters": "power,power_p10,power_p90",
+    "latitude": 6.9271,    
+    "longitude": 79.8612,
+    "output_parameters": "ghi,dni,dhi,air_temp",
     "period": "PT5M",
-    "hours": 24,
+    "hours": 336,
     "format": "json"
 }
 
-try:
-    print("🔄 Fetching data from Solcast...")
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
 
-    response = requests.get(url, params=params)
-    print(response.url)
-    
-    response.raise_for_status()
+response = requests.get(url, params=params, headers=headers)
+response.raise_for_status()
+data = response.json()
+df = pd.DataFrame(data["forecasts"])
+print(df.head())
 
-    data = response.json()
-    forecasts = data["forecasts"]
+# Create the output folder if it doesn't exist
+os.makedirs("data", exist_ok=True)
 
-    print("✅ Data received!\n")
+# Get the current Sri Lankan time
+timestamp = datetime.now(
+    ZoneInfo("Asia/Colombo")
+).strftime("%Y-%m-%d_%H-%M")
 
-    # Print first 10 rows
-    for f in forecasts[:10]:
-        print(f"{f['period_end']} -> {f['power']} kW")
+# Create the filename
+filename = f"data/irradiance_forecast_{timestamp}.csv"
 
-    # Save CSV
-    # Create folder if it doesn't exist
-    os.makedirs("data", exist_ok=True)
+# Save the CSV
+df.to_csv(filename, index=False)
 
-    # Sri Lankan time
-    timestamp = datetime.now(ZoneInfo("Asia/Colombo")).strftime("%Y-%m-%d_%H-%M")
-
-    filename = f"data/forecast_{timestamp}.csv"
-
-    with open(filename, "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Time", "Power", "P10", "P90"])
-
-        for f in forecasts:
-            writer.writerow([
-                f["period_end"],
-                f["power"],
-                f["power_p10"],
-                f["power_p90"]
-            ])
-
-    print(f"\n✅ CSV file saved as {filename}")
-
-except requests.exceptions.RequestException as e:
-    print("Error:", e)
+print(f"Saved to {filename}")
