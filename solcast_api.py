1bf098ebdd44455fb6da1f1d8f803c9f4913de69
PrajeshS <89408105+PrajeshS@users.noreply.github.com>
2026-07-09 10:36:45 +0530

    Update solcast_api.py

--- solcast_api.py
+++ solcast_api.py
@@ -5,9 +5,9 @@ import msal
 from datetime import datetime
 from zoneinfo import ZoneInfo
 API_KEY = "Ltmj-pqWaCVKTAni0yrsexlj3ZKMUv_S"
-CLIENT_ID = os.getenv["ONEDRIVE_CLIENT_ID"]
-CLIENT_SECRET = os.getenv["ONEDRIVE_CLIENT_SECRET"]
-TENANT_ID = os.getenv["ONEDRIVE_TENANT_ID"]
+CLIENT_ID = os.environ["ONEDRIVE_CLIENT_ID"]
+CLIENT_SECRET = os.environ["ONEDRIVE_CLIENT_SECRET"]
+TENANT_ID = os.environ["ONEDRIVE_TENANT_ID"]
 # Download irradiance forecast
 url = "https://api.solcast.com.au/data/forecast/radiation_and_weather"
 params = {
