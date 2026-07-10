fb6fae98b908acd704d89ff300e2e58b15547fc2
PrajeshS <89408105+PrajeshS@users.noreply.github.com>
2026-07-10 10:07:12 +0530

    Update solcast_api.py

--- solcast_api.py
+++ solcast_api.py
@@ -55,7 +55,6 @@ df["period_end"] = (
       .dt.tz_convert("Asia/Colombo")
       .dt.strftime("%Y-%m-%d %H:%M:%S")
 )
-print(df.head())
 # Create the output folder if it doesn't exist
 os.makedirs("data", exist_ok=True)
 # Get the current Sri Lankan time
